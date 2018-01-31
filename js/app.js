d9cd979d914d0fffd4db21fa8269f08d8bebf669
Dan Aschenbach <phily_phreak@yahoo.com>
2018-01-31 11:38:01 -0600

    feat: global vars

---
+++ js/app.js
@@ -0,0 +1,9 @@
+	//setup globals
+var store;
+var vm;
+var viewModel;
+	//store list will later be in database and much longer
+var mySearch = [
+	{name: "eBay", type: "Online", webLink: {https://www.ebay.com/}},
+	{name: "O'Reilly's", type: "In-Store", webLink: {https://www.oreillyauto.com/}},
+];
