dc0a1df2bb928dadf00251867ee2b505e6a3d8ca
Dan Aschenbach <phily_phreak@yahoo.com>
2018-01-31 12:20:07 -0600

    feat: begin viewModel and 1st filter

--- js/app.js
+++ js/app.js
@@ -7,3 +7,47 @@ var mySearch = [
 	{name: "eBay", type: "Online", webLink: {https://www.ebay.com/}},
 	{name: "O'Reilly's", type: "In-Store", webLink: {https://www.oreillyauto.com/}},
 ];
+							 
+	//load clicked store to main content
+function initShow() {
+	store = new ???({ //getting back to this
+	});
+
+	//makes list clickable & ties together with prices
+	var Pin = function(data) {
+		this.name = ko.observable(data.name);
+		this.type = ko.observable(data.type);
+	};
+
+	var ViewModel = function() {
+		var self = this;
+
+		this.url = ko.observable();
+		this.title = ko.observable();
+		this.placeLinks = ko.observable();
+		this.pinList = ko.observableArray([]);
+		this.currentPin = ko.observable(this.pinList()[0]);
+	// Modified filter function, original by Blake Watson on codepen
+		self.myStores = myStores;
+		self.selectedType = ko.observable("Both");
+		self.filteredmyStores = ko.computed(function(pinItem) {
+			var type = self.selectedType();
+			console.log(self.selectedType);
+			if(type === "Both") {
+				console.log(type);
+				self.myStores.forEach(function(myStores) {
+					if(myStores.prices)myStores.prices.setVisible(true);
+				});
+
+	// iterate over self.myStores & set prices to visible
+				return self.myStores;
+			} else {
+				var tempList = self.myStores.slice();
+				return tempList.filter(function(myStore) {
+					var match = myStore.type === type;
+					console.log(myStore);
+					myStore.price.setVisible(match);
+					return match;
+				});
+			}
+		});							 
